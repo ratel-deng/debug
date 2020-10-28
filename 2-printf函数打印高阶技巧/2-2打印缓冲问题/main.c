ade05dc896e3cc6e82532044e329ead93d59bf1d
Rick <1952630850@qq.com>
2020-10-28 18:17:23 +0800

    打印缓冲问题

---
+++ 2-printf函数打印高阶技巧/2-2打印缓冲问题/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+	printf("demo1...\n");
+	sleep(1);//延时一秒
+	printf("demo2...\n");
+	sleep(1);//延时一秒
+	printf("demo3...");
+	sleep(1);//延时一秒
+	printf("demo4...");
+	sleep(1);//延时一秒
+	printf("demo5...");
+	sleep(1);//延时一秒
+	return 0;
+}
+
